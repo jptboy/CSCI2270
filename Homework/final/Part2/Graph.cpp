80cbb02646595c868b3edf32a50348ca0dd26da0
jptboy <vegetablebat1@gmail.com>
2018-07-26 00:12:39 -0700

    done with everything but djikstras

---
+++ Homework/final/Part2/Graph.cpp
@@ -0,0 +1,259 @@
+#include <iostream>
+#include "Graph.hpp"
+using namespace std;
+Graph::Graph()
+{
+
+};
+Graph::~Graph()
+{
+
+};
+void Graph::addEdge(std::string v1, std::string v2, int weight)
+{
+    unsigned int cityIndex = stoi(v2);
+    /*
+    cout
+        << "Edge length of: "<< weight <<" between: " << v1
+        << " and: " << vertices[cityIndex].name << endl;
+    */
+    adjVertex *newAdjVertex = new adjVertex;
+    newAdjVertex->weight = weight;
+    newAdjVertex->v = &(vertices[cityIndex]);
+
+    unsigned int startingIdx=0;
+    bool found = false;
+    for(unsigned int i=0; i<vertices.size(); i++)
+    {
+        if(vertices[i].name==v1)
+        {
+            startingIdx=i;
+            found=true;
+            break;
+        }
+    }
+    if(found)
+    {
+        vertices[startingIdx].adj.push_back(*newAdjVertex);
+        //cout << "Edge has been added." << endl;
+        delete newAdjVertex;
+    }
+    else
+    {
+        cout << "Huge error" << endl;
+    }
+
+}
+void Graph::addVertex(std::string name)
+{
+    vertex *newVertex = new vertex;
+    newVertex->name = name;
+    newVertex->districtID=-1;
+    newVertex->ID=vertices.size();
+    vertices.push_back(*newVertex);
+    delete newVertex;
+}
+void Graph::displayEdges()
+{
+    for(unsigned int i=0; i<vertices.size(); i++)
+    {
+        cout << vertices[i].districtID << ":" << vertices[i].name << "->";
+        for(unsigned int j=0; j<(vertices[i].adj.size()); j++)
+        {
+            cout << vertices[i].adj[j].v->name;
+            if(j!=vertices[i].adj.size()-1)
+                cout << "***";
+        }
+        cout << endl;
+    }
+}
+
+void Graph::assignDistricts()
+{
+    int currentDistrictId = 1;
+    for(unsigned int i=0; i<vertices.size(); i++)
+    {
+        if(!(vertices[i].visited))
+        {
+            DFSLabel(vertices[i].name, currentDistrictId);
+            currentDistrictId++;
+            //cout << "------"  << endl;
+        }
+    }
+}
+void Graph::shortestPath(std::string startingCity, std::string endingCity)
+{
+    for(unsigned int i=0; i<vertices.size();i++)
+    {
+        vertices[i].visited = false;
+        vertices[i].unweightedDistance = 0;
+        vertices[i].parent = NULL;
+    }
+
+
+
+
+    vertex* firstcity;
+    vertex* secondcity;
+
+    bool found = false;
+    bool found2 = false;
+    for(unsigned int i=0; i<vertices.size(); i++)
+    {
+        if(vertices[i].districtID==-1)
+        {
+            cout << "Please identify the districts before checking distances" << endl;
+            return;
+        }
+        if(vertices[i].name == startingCity)
+        {
+            firstcity = &vertices[i];
+            found = true;
+        }
+        if(vertices[i].name == endingCity)
+        {
+            secondcity = &vertices[i];
+            found2 = true;
+        }
+    }
+    if(!(found && found2))
+    {
+        cout << "One or more of the cities does not exist" << endl;
+        return;
+    }
+    else if(firstcity->districtID!=secondcity->districtID)
+    {
+        cout << "No safe path between cities" << endl;
+        return;
+    }
+
+    firstcity->unweightedDistance=0;
+    vector<vertex*> queue;
+    queue.push_back(firstcity);
+    vertex* currentcity = queue[0];
+    while(queue.size()>0)
+    {
+        currentcity = queue[0];
+        //queue.erase(queue.begin());
+        currentcity->visited = true;
+
+        vector<vertex*> unvisiteds;
+        for(unsigned int i=0; i < currentcity->adj.size(); i++)
+        {
+            if(currentcity->adj[i].v->visited==false)
+                unvisiteds.push_back(currentcity->adj[i].v);
+        }
+
+        if(unvisiteds.size()==0)
+        {
+            queue.erase(queue.begin());
+        }
+        else
+        {
+            vertex dummy;
+            string dummystr = "ZZZZZZZZZZZZZZZZZZZZ";
+            dummy.name=dummystr;
+            vertex* leastvertex = &(dummy);
+
+            for(unsigned int i=0; i < unvisiteds.size(); i++)
+            {
+                if(unvisiteds[i]->name<leastvertex->name)
+                    leastvertex=unvisiteds[i];
+            }
+            leastvertex->visited=true;
+            leastvertex->parent=currentcity;
+            leastvertex->unweightedDistance= (currentcity->unweightedDistance)+1;
+            //cout << "visited: " << leastvertex->name << endl;
+            queue.push_back(leastvertex);
+        }
+
+    }
+
+    int shortestdistance = secondcity->unweightedDistance;
+    vector<vertex*>path;
+    vertex* temp= secondcity;
+    while(temp->name!=firstcity->name)
+    {
+        path.insert(path.begin(), temp);
+        temp = temp->parent;
+    }
+    path.insert(path.begin(), firstcity);
+    temp=NULL;
+    cout << shortestdistance;
+    for(unsigned int i=0; i<path.size(); i++)
+    {
+        cout << "," << path[i]->name;
+    }
+    cout << endl;
+}
+void Graph::shortestWeightedPath(std::string startingCity,
+    std::string endingCity)
+{
+
+}
+vertex* Graph::findVertex(std::string name)
+{
+    for(unsigned int i=0; i<vertices.size(); i++)
+    {
+        if(vertices[i].name == name)
+            return &(vertices[i]);
+    }
+    return NULL;
+}
+void Graph::DFSLabel(std::string startingCity, int distID)
+{
+    vector<vertex*> stack;
+    vertex* startingVert = NULL;
+    for(unsigned int i=0; i<vertices.size(); i++)
+    {
+        if(vertices[i].name==startingCity)
+        {
+            startingVert = &(vertices[i]);
+        }
+    }
+    stack.push_back(startingVert);
+    while(stack.size()>0)
+    {
+        string dummystring = "ZZZZZZZZZZZZ";
+        vertex dummyvertex;
+        dummyvertex.name=dummystring;
+        vertex* comparevertex = &dummyvertex;
+        vertex* currVert = stack.back();
+        currVert->visited=true;
+        //cout << currVert->name << " visited" << endl;
+        currVert->districtID=distID;
+        /*Above we just set crap and work with the vertex at the top of the
+        stack*/
+        bool allAdjsVisited = true;
+        for(unsigned int i=0; i<currVert->adj.size(); i++)
+        {
+            if(!(currVert->adj[i].v->visited))
+                allAdjsVisited = false;
+        }
+        if(allAdjsVisited)
+            stack.pop_back();
+        /*
+        Above we are checking if all the adjacents have been visited
+        if they have, we want to pop the current vertex off the stack
+        */
+        else
+        {
+            for(unsigned int i=0; i<currVert->adj.size(); i++)
+            {
+                if(!(currVert->adj[i].v->visited) && (currVert->adj[i].v->name
+                    < comparevertex->name))
+                {
+                    comparevertex = currVert->adj[i].v;
+                }
+            }
+            /*
+            Above we are finding the vertex with the least alphabetical val
+            */
+            if(comparevertex->name!=dummystring)
+                stack.push_back(comparevertex);
+        }
+
+
+
+    }
+}
---
+++ Homework/final/Part2/Part2.cpp
@@ -0,0 +1,143 @@
+#include <iostream>
+#include <fstream>
+#include "Graph.hpp"
+#include <sstream>
+#include <string>
+using namespace std;
+int splitLine(string line, string *splitLineArrayptr,string **memaddressofptrtoarray, char sep)//this function is from homework2 and it  is used to split individual lines in a text file
+{
+    stringstream splitter(line);
+    string word= "";
+    int arrayitemcount=0;
+    int arraysize=2;
+    while(getline(splitter,word, sep))
+    {
+        if(arrayitemcount==arraysize)//we double the array if the number of elements has matched the size
+        {
+            arraysize*=2;
+            string *temp = NULL;
+            temp=new string[arraysize];
+            for(int i=0; i<arraysize;i++)
+            {
+                temp[i]="NULL";
+            }
+            for(int i=0; i<arrayitemcount; i++)
+            {
+                temp[i]=splitLineArrayptr[i];
+            }
+            delete [] splitLineArrayptr;
+            splitLineArrayptr=temp;
+            temp=NULL;//we do t his to not have dangling pointers
+        }
+        splitLineArrayptr[arrayitemcount++]=word;//otherwise we keep filling it up
+    }
+    string *correctsizearray = NULL;
+    correctsizearray=new string[arrayitemcount];
+    for(int i=0; i<arrayitemcount; i++)
+    {
+        correctsizearray[i]=splitLineArrayptr[i];
+    }
+    delete [] splitLineArrayptr;
+    splitLineArrayptr=NULL;
+    *memaddressofptrtoarray=correctsizearray;//we derefrence the pointer to the array given and set it equal to the new array with exactly the correct number of split words from the line
+    return arrayitemcount;
+}
+void mainMenu(string fileName)
+{
+    Graph cities;
+    ifstream filein(fileName);
+    string line;
+    for(int i=0;getline(filein,line);i++)
+    {
+        string *lineholder = NULL;
+        lineholder = new string[2];
+        int linelength = splitLine(line, lineholder, &lineholder, ',');
+        if(i==0)
+        {
+            for(int j=1;j<linelength;j++)
+            {
+                cities.addVertex(lineholder[j]);
+            }
+        }
+        else
+        {
+            for(int j=1; j<linelength; j++)
+            {
+                if(lineholder[j]!="0" && lineholder[j]!="-1")
+                {
+                    cities.addEdge(
+                        lineholder[0], to_string(j-1), stoi(lineholder[j])
+                    );
+
+                }
+            }
+        }
+        delete [] lineholder;
+        lineholder = NULL;
+    }
+
+
+
+    string option="";
+    while(option!="5")//infinite loop until option equal to 3 for bringing people back to the menu
+
+    //The contents of this while loop is just the menu stuff and taking user input, nothing special.
+    //Just a series of if else if statements over and over until the user presses 3 and quits.
+
+    {
+        cout << "======Main Menu======" << endl;
+        cout << "1. Print vertices" << endl;
+        cout << "2. Find districts" << endl;
+        cout << "3. Find shortest path" << endl;
+        cout << "4. Find shortest weighted path" << endl;
+        cout << "5. Quit" << endl;
+
+        getline(cin,option);
+
+        if(option=="1")
+        {
+            cities.displayEdges();
+        }
+        else if(option=="2")
+        {
+            cities.assignDistricts();
+        }
+        else if(option=="3")
+        {
+            string first;
+            string second;
+            cout << "Enter a starting city:" << endl;
+            getline(cin, first);
+            cout << "Enter an ending city:" << endl;
+            getline(cin, second);
+
+            cities.shortestPath(first, second);
+        }
+        else if(option=="4")
+        {
+
+        }
+        else if(option=="5")
+        {
+            cout << "Goodbye!" << endl;
+        }
+        else
+        {
+            cout << "Error please enter a valid option!" << endl;
+        }
+
+    }
+}
+int main(int argc, char const *argv[])
+{
+    if(argc<2)//to prevent errors if less than number of required
+    //command line arguments
+    {
+        cout
+            << "Please give a proper number of arguments! Program will now exit!"
+            << endl;
+        return 1;
+    }
+    mainMenu(argv[1]);
+    return 0;
+}
